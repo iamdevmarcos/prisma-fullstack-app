efed3913552cc59e20fdff40774f7c6f888f9982
Marcos Andre <marcosdev.me@gmail.com>
2022-10-19 20:33:47 -0300

    add graphql schema and resolvers

---
+++ graphql/resolvers.ts
@@ -0,0 +1,23 @@
+export const resolvers = {
+  Query: {
+    links: () => [
+      {
+        category: "Open Source",
+        description: "Fullstack React framework",
+        id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
+        imageUrl: "https://blog.rocketseat.com.br/content/images/size/w1000/2018/12/ssr-nextjs-reactjs.png",
+        title: "Next.js",
+        url: "https://nextjs.org",
+      },
+      {
+        category: "Open Source",
+        description: "Next Generation ORM for TypeScript and JavaScript",
+        id: "2a3121b2-363b-4a4f-ad26-d6c35b41bade",
+        imageUrl: "https://www.prisma.io/images/og-image.png",
+    
+        title: "Prisma",
+        url: "https://prisma.io",
+      },
+    ]
+  }
+}
