b19311a4d70c780c737357bd55e22c5861cc1b80
Marcos Andre <marcosdev.me@gmail.com>
2022-10-19 20:55:38 -0300

    creating a graphql context

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,7 +1,5 @@
-import prisma from "../lib/prisma"
-
 export const resolvers = {
   Query: {
-    links: async () => await prisma.link.findMany()
+    links: async (_parent, _args, ctx) => await ctx.prisma.link.findMany()
   }
 }
