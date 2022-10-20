1ffc91aff99b8071c05ad43b88ac6e2ff223b1f8
Marcos Andre <marcosdev.me@gmail.com>
2022-10-20 08:42:13 -0300

    define graphql query

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,4 +1,4 @@
-import { objectType } from 'nexus';
+import { extendType, objectType } from 'nexus';
 import { User } from './User'
 
 export const Link = objectType({
@@ -25,4 +25,16 @@ export const Link = objectType({
       }
     })
   },
+});
+
+export const LinksQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.field('links', {
+      type: 'Link',
+      resolve(_parent, _args, ctx) {
+        return ctx.prisma.link.findMany()
+      }
+    })
+  }
 });
